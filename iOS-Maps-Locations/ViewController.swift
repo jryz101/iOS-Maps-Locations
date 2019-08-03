916451091e6d9529c1eaa4012a2032112dbf6929
JerryTan <jryz101@gmail.com>
2019-08-03 18:41:17 +0800

    3. Declare CLLocationDegrees latitude and longitude, latDelta and longDelta, MKCoordinateSpan, CLLocationCoordinate2D, and MKCoordinateRegion. Finally, set the map.setRegion.

--- iOS-Maps-Locations/ViewController.swift
+++ iOS-Maps-Locations/ViewController.swift
@@ -17,7 +17,28 @@ class ViewController: UIViewController, MKMapViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //A latitude or longitude value specified in degrees | Map View Level
+        let latitude: CLLocationDegrees = 40.8
+        let longitude: CLLocationDegrees = -73.9
+        
+        //A latitude or longitude value specified in degrees | Map Zoom In Level
+        let latDelta: CLLocationDegrees = 0.05
+        let longDelta: CLLocationDegrees = 0.05
+        
+        //The width and height of a map region | Combination of latDelta & longDelta
+        let span: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: longDelta)
+        
+        //The latitude and longitude associated with a location, specified using the WGS 84 reference frame | Combination of latitude & longitude
+        let location: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        
+        //A rectangular geographic region centered around a specific latitude and longitude | Combination of location & span
+        let region: MKCoordinateRegion = MKCoordinateRegion(center: location, span: span)
+        
+        //Changes the currently visible region and optionally animates the change | Set map region
+        map.setRegion(region, animated: true)
+        
+        
     }
 }
 
