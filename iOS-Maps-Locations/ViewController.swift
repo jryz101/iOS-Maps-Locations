55989df12438b002639a2570e7ddf91c218f7f4d
JerryTan <jryz101@gmail.com>
2019-08-03 18:16:52 +0800

    2. Imported MapKit framework, create MKMapView IB outlet, and declare MKMapViewDelegate protocol.

--- iOS-Maps-Locations/ViewController.swift
+++ iOS-Maps-Locations/ViewController.swift
@@ -1,20 +1,23 @@
-//
 //  ViewController.swift
 //  iOS-Maps-Locations
-//
 //  Created by Jerry Tan on 03/08/2019.
 //  Copyright © 2019 Starknet Technologies®. All rights reserved.
-//
+
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet weak var map: MKMapView!
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
 }
 
